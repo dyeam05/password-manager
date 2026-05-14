3d3c99b3d4b34ae76cc573923f6649966248afa1
dyeam05 <danielyeaman05@gmail.com>
2026-05-13 21:04:59 -0500

    adds canvas window to display stored login info

--- src/main-gui.py
+++ src/main-gui.py
@@ -1,6 +1,8 @@
 import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 import hashlib
-import file_check
+import file_handler
 
 logged_in = False
 
@@ -12,13 +14,25 @@ def login():
     print(f"user name: {user_input}")
     print(f"pass input: {pass_input}")
     print(f"hashed pass: {hex_pass}")
-    validLogin = file_check.verify_info(user_input, hex_pass)
+    validLogin = file_handler.verify_info(user_input, hex_pass)
     if validLogin:
         print("successful login")
+        showData()
         
     else:
         incorrectLogin.pack()
 
+def showData():
+    userEntry.pack_forget()
+    passEntry.pack_forget()
+    submitBtn.pack_forget()
+    incorrectLogin.pack_forget()
+    canvas.pack() #side=LEFT, fill=BOTH, expand=True
+    #scrollbar.pack(side=RIGHT, fill=Y)
+    serviceLabel = canvas.create_text(100, 100, text="Service")
+    userLabel = canvas.create_text(300, 100, text="Username")
+    passLabel = canvas.create_text(500, 100, text="Password")
+    
 
 root = tk.Tk()
 root.title("Password Manager")
@@ -31,9 +45,24 @@ userEntry = tk.Entry(root, width=50)
 passEntry = tk.Entry(root, width=50, show="*")
 submitBtn = tk.Button(root, text="Submit", command=login)
 incorrectLogin = tk.Label(root, text="Incorrect Password or Username", fg="red")
-
 userEntry.pack(pady=10)
 passEntry.pack(pady=10)
 submitBtn.pack()
+
+
+canvas = Canvas(root, width=600, height=600, bg="#c5c5c5")
+
+"""
+scrollbar = Scrollbar(root, orient=VERTICAL, command=canvas.yview)
+
+canvas.configure(yscrollcommand=scrollbar.set)
+scrollable_frame = Frame(canvas)
+
+canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
+scrollable_frame.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+"""
+
+
+
 
 root.mainloop()
